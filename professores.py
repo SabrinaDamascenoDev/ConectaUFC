fca5b03b926626792b6278f78459c566ce81fee2
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2025-02-24 19:08:26 -0300

    Comentários

--- professores.py
+++ professores.py
@@ -8,20 +8,22 @@ response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'lxml')
 
-nome_professores = []
+professoresDados = soup.find_all("div", class_="row")
 
-nomes_professores = soup.find_all("div", class_="row")
-print(nomes_professores)
-for professores in nomes_professores:
+# Pegando nomes
+nome_professores = []
+for professores in professoresDados:
    nome_professores.append(professores.find("h2").text.strip() if professores.find("h2") else "nao")
 
+# Pegando comentários
 comentario_professores = []
-
-for comentario in nomes_professores:
+for comentario in professoresDados:
    comentario_professores.append(comentario.find("p").text.strip() if comentario.find("p") else "nao")
 
 texto_sem_quebra = [i.replace("\n", " ") for i in comentario_professores]
+# Falta pegar o link da foto e associar
 
+# Associando dados em um dic
 associando_professores = {}
 for i in range(len(nome_professores)):
    associando_professores[nome_professores[i]] = texto_sem_quebra[i]
